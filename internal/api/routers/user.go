3e84348d27c4752d0ebafb79750a2707ef85e7e7
metalgear-dev <dantes0121@hotmail.com>
2023-03-30 16:12:11 +0800

    add caches for user APIs

--- internal/api/routers/user.go
+++ internal/api/routers/user.go
@@ -2,17 +2,22 @@ package routers
 
 import (
 	"andromeda/internal/api/controllers"
+	"time"
 
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRouter(r *gin.RouterGroup) {
+	userStore := persistence.NewInMemoryStore(time.Second)
 	userController := new(controllers.User)
+
 	c := r.Group("/users")
 	c.Use()
 	{
-		c.GET("/activities", userController.GetActivities)
-		c.GET("/nfts", userController.GetNFTs)
-		c.GET("/offers", userController.GetOffers)
+		c.GET("/activities", cache.CachePage(userStore, time.Second*30, userController.GetActivities))
+		c.GET("/nfts", cache.CachePage(userStore, time.Minute, userController.GetNFTs))
+		c.GET("/offers", cache.CachePage(userStore, time.Second*30, userController.GetOffers))
 	}
 }
