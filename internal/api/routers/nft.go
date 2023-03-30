dadd5f81278518fee74da10d01c61bad68552b5b
metalgear-dev <dantes0121@hotmail.com>
2023-03-30 16:04:44 +0800

    add cache for nft APIs

--- internal/api/routers/nft.go
+++ internal/api/routers/nft.go
@@ -2,17 +2,22 @@ package routers
 
 import (
 	"andromeda/internal/api/controllers"
+	"time"
 
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 )
 
 func NFTRouter(r *gin.RouterGroup) {
+	nftStore := persistence.NewInMemoryStore(time.Second)
 	nftController := new(controllers.NFT)
+
 	c := r.Group("/nfts")
 	c.Use()
 	{
-		c.GET("/activities", nftController.GetActivities)
-		c.GET("/offers", nftController.GetOffers)
-		c.GET("/:address", nftController.GetDetail)
+		c.GET("/activities", cache.CachePage(nftStore, time.Second*10, nftController.GetActivities))
+		c.GET("/offers", cache.CachePage(nftStore, time.Second*10, nftController.GetOffers))
+		c.GET("/:address", cache.CachePage(nftStore, time.Second*10, nftController.GetDetail))
 	}
 }
