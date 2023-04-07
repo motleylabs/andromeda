d4a5258411b612ed63976bb678610e1fc8c4cc6e
metalgear-dev <dantes0121@hotmail.com>
2023-04-07 13:37:54 +0800

    added tests for other collection APIs

--- internal/api/test/collection/collection_test.go
+++ internal/api/test/collection/collection_test.go
@@ -4,6 +4,7 @@ import (
 	"andromeda/internal/api/routers"
 	"andromeda/pkg/service/entrance/types"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -36,25 +37,88 @@ func init() {
 	if err := json.Unmarshal(data, &testCase); err != nil {
 		log.Fatalf("Test case file format is wrong")
 	}
+
+	callPreliminaryAPI()
 }
 
-func ProcessGet(url string) (int, []byte) {
-	req, _ := http.NewRequest("GET", url, nil)
+func processGet(url string) (int, []byte) {
+	req, _ := http.NewRequest("GET", fmt.Sprintf("/api/%s", url), nil)
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 	responseData, _ := io.ReadAll(w.Body)
 	return w.Code, responseData
 }
 
+func callPreliminaryAPI() {
+	preliminaryURL := testCase["preliminary"]
+	if len(preliminaryURL) > 0 {
+		processGet(preliminaryURL[0])
+	} else {
+		log.Fatalf("Preliminary URL is missing")
+	}
+}
+
 func TestTrends(t *testing.T) {
 	testURLs := testCase["trends"]
 
 	for _, testURL := range testURLs {
-		statusCode, resBytes := ProcessGet(testURL)
+		statusCode, resBytes := processGet(testURL)
 		assert.Equal(t, 200, statusCode)
 
 		var trendRes types.TrendRes
 		err := json.Unmarshal(resBytes, &trendRes)
+		assert.Nil(t, err)
+	}
+}
+
+func TestDetail(t *testing.T) {
+	testURLs := testCase["detail"]
+
+	for _, testURL := range testURLs {
+		statusCode, resBytes := processGet(testURL)
+		assert.Equal(t, 200, statusCode)
+
+		var detailRes types.Collection
+		err := json.Unmarshal(resBytes, &detailRes)
+		assert.Nil(t, err)
+	}
+}
+
+func TestTimeseries(t *testing.T) {
+	testURLs := testCase["timeseries"]
+
+	for _, testURL := range testURLs {
+		statusCode, resBytes := processGet(testURL)
+		assert.Equal(t, 200, statusCode)
+
+		var seriesRes types.TimeSeriesRes
+		err := json.Unmarshal(resBytes, &seriesRes)
+		assert.Nil(t, err)
+	}
+}
+
+func TestNFTs(t *testing.T) {
+	testURLs := testCase["nfts"]
+
+	for _, testURL := range testURLs {
+		statusCode, resBytes := processGet(testURL)
+		assert.Equal(t, 200, statusCode)
+
+		var nftRes types.NFTRes
+		err := json.Unmarshal(resBytes, &nftRes)
+		assert.Nil(t, err)
+	}
+}
+
+func TestActivities(t *testing.T) {
+	testURLs := testCase["activities"]
+
+	for _, testURL := range testURLs {
+		statusCode, resBytes := processGet(testURL)
+		assert.Equal(t, 200, statusCode)
+
+		var activityRes types.ActivityRes
+		err := json.Unmarshal(resBytes, &activityRes)
 		assert.Nil(t, err)
 	}
 }
