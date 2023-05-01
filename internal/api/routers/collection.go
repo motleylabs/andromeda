d099ca8beeb2f79db7cad5a7b53d823a4c7f1401
omar <omar.kilani@gmail.com>
2023-04-30 20:54:47 -0700

    Remove gin-cache.

--- internal/api/routers/collection.go
+++ internal/api/routers/collection.go
@@ -2,25 +2,20 @@ package routers
 
 import (
 	"andromeda/internal/api/controllers"
-	"time"
-
-	"github.com/gin-contrib/cache"
-	"github.com/gin-contrib/cache/persistence"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CollectionRouter(r *gin.RouterGroup) {
-	collectionStore := persistence.NewInMemoryStore(time.Second)
 	collectionController := new(controllers.Collection)
 
 	c := r.Group("/collections")
 	c.Use()
 	{
-		c.GET("/trend", cache.CachePage(collectionStore, time.Minute, collectionController.GetTrends))
-		c.GET("/series", cache.CachePage(collectionStore, time.Minute*30, collectionController.GetTimeSeries))
-		c.GET("/nfts", cache.CachePage(collectionStore, time.Minute, collectionController.GetNFTs))
-		c.GET("/activities", cache.CachePage(collectionStore, time.Minute, collectionController.GetActivities))
-		c.GET("/:address", cache.CachePage(collectionStore, time.Hour, collectionController.GetDetail))
+		c.GET("/trend", collectionController.GetTrends)
+		c.GET("/series", collectionController.GetTimeSeries)
+		c.GET("/nfts", collectionController.GetNFTs)
+		c.GET("/activities", collectionController.GetActivities)
+		c.GET("/:address", collectionController.GetDetail)
 	}
 }
--- internal/api/routers/stats.go
+++ internal/api/routers/stats.go
@@ -2,21 +2,17 @@ package routers
 
 import (
 	"andromeda/internal/api/controllers"
-	"time"
 
-	"github.com/gin-contrib/cache"
-	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 )
 
 func StatRouter(r *gin.RouterGroup) {
-	statStore := persistence.NewInMemoryStore(time.Second)
 	statController := new(controllers.Stat)
 
 	c := r.Group("/stat")
 	c.Use()
 	{
-		c.GET("/overall", cache.CachePage(statStore, time.Minute*5, statController.GetOverallStat))
+		c.GET("/overall", statController.GetOverallStat)
 		c.GET("/search", statController.Search)
 	}
 }
